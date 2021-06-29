9e2db2bf01a614f8a58de9584aa127411c96ea40
Your Name <r.hasan07@gmail.com>
2021-06-29 16:46:32 +0600

    upto 09-03

--- app.js
+++ app.js
@@ -18,11 +18,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -31,6 +26,13 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.get('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  })
+})
 
 // 4) START SERVER
 module.exports = app;
