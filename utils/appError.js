ac335730f03b63c14a3cdd2f46066d26652460e6
Your Name <r.hasan07@gmail.com>
2021-06-29 18:19:18 +0600

    upto 09-05

---
+++ utils/appError.js
@@ -0,0 +1,9 @@
+class AppError extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.status = `${statusCode}.startsWith('4')? 'fail' : 'error'`;
+        this.isOperational = true;
+
+        Error.captureStackTrace(this);
+    }
+}
