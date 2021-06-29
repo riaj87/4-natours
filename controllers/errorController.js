df19a03baf6b6eb63c9d86e3c9f0b880c19e3739
riaj87 <r.hasan07@gmail.com>
2021-06-30 00:16:13 +0600

    upto 09-06

---
+++ controllers/errorController.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-expressions */
+module.exports = (err, req, res, next) => {
+  console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
