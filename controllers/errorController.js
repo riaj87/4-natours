1196d10db479e2430b105bbf2828dfab91b5301d
Your Name <r.hasan07@gmail.com>
2021-06-30 17:02:14 +0600

    Cont. 09-09

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,13 +1,43 @@
 /* eslint-disable no-console */
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-unused-expressions */
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+}
+
+const sendErrorProd = (err, res) => {
+  // Operatinal, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+    // Programming or other unknown error: don't leak error details
+  } else {
+
+    // 2) Send generic error
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    })
+
+  }
+}
 module.exports = (err, req, res, next) => {
-  console.log(err.stack);
+  // console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
+
+
 };
