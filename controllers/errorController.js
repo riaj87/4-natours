9bb0eba522ef88117df115ec939bd811f062df6d
riaj87 <r.hasan07@gmail.com>
2021-07-01 01:54:31 +0600

    upto 09-12

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,33 +1,57 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-useless-path-segments */
+/* eslint-disable node/no-unsupported-features/es-syntax */
+/* eslint-disable no-undef */
 /* eslint-disable no-console */
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-unused-expressions */
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  console.log(value);
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
     message: err.message,
-    stack: err.stack
+    stack: err.stack,
   });
-}
+};
 
 const sendErrorProd = (err, res) => {
   // Operatinal, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
-      message: err.message
+      message: err.message,
     });
     // Programming or other unknown error: don't leak error details
   } else {
+    // 1) Log error
+    console.log('ERROR', err);
 
     // 2) Send generic error
     res.status(500).json({
       status: 'error',
-      message: 'Something went very wrong!'
-    })
-
+      message: 'Something went very wrong!',
+    });
   }
-}
+};
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
   err.statusCode = err.statusCode || 500;
@@ -36,8 +60,12 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
   }
-
-
 };
