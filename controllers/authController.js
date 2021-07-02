62e1cb830e8ea67d6c40db13f1268caf53445202
riaj87 <r.hasan07@gmail.com>
2021-07-02 23:01:32 +0600

    upto 10-06

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,19 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/no-useless-path-segments */
+const jwt = require('jsonwebtoken');
 const User = require('./../Models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
