000ac7754fca7310875335ad6bde39a7556eecfc
riaj87 <r.hasan07@gmail.com>
2021-07-02 16:40:32 +0600

    upto 10-04

--- Models/userModel.js
+++ Models/userModel.js
@@ -3,6 +3,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // name, email, photo, password, passwordConfirm
 const userSchema = new mongoose.Schema({
@@ -28,7 +29,24 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same! ',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+  // Hash the password with the cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // Delete password confirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
