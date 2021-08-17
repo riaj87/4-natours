f21abcd595c93e68cef629f9dd30bf7406a65388
riaj87 <r.hasan07@gmail.com>
2021-08-17 21:20:59 +0600

    upto 10-17

--- Models/userModel.js
+++ Models/userModel.js
@@ -49,6 +49,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   PasswordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre('save', async function (next) {
@@ -65,6 +70,12 @@ userSchema.pre('save', function (next) {
   if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  // this points to the current query
+  this.find({ active: { $ne: false } });
   next();
 });
 
