ffc428031798c3fa495031f5edadf58f4ed1e96f
riaj87 <r.hasan07@gmail.com>
2021-08-17 17:46:11 +0600

    upto 10-16

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,6 +15,7 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
+router.patch('/updateMe', authController.protect, userController.updateMe);
 
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 
