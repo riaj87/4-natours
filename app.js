9768d317bae8e8e05a048adf867926924a91bc04
riaj87 <r.hasan07@gmail.com>
2021-08-21 23:22:00 +0600

    upto 10-23

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -32,8 +35,22 @@ app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
 
 // Data sanitization against noSQL query injection
-
-// Data sanitization against XSS
+app.use(mongoSanitize());
+//  Data sanitization against XSS
+app.use(xss());
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
