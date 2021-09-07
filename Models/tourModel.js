0155ca3d4bbe2f82298ce646ac3a26b8ae628df3
Your Name <r.hasan07@gmail.com>
2021-09-07 17:12:31 +0600

    upto 11-09

--- Models/tourModel.js
+++ Models/tourModel.js
@@ -147,8 +147,15 @@ tourSchema.pre('save', function (next) {
 tourSchema.pre(/^find/, function (next) {
   // tourSchema.pre('find', function (next) {
   this.find({ secretTour: { $ne: true } });
-
   this.start = Date.now();
+  next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  });
   next();
 });
 
@@ -156,6 +163,7 @@ tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
   next();
 });
+
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function (next) {
